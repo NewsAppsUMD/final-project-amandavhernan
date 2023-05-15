4118e9994ed989d630e407f1ee59ebeef044666f
Amanda Hernández <amandavhernan@gmail.com>
2023-05-15 21:59:15 +0000

    app updates

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # data
-    pfas = pd.read_csv('data/geocodio_known_sites.csv')
+    
+    # load data
+    pfas = pd.read_csv('data/known_sites_clean.csv')
 
+    # folium map basics
     start_latlon = (39.50, -98.35)
     folium_map = folium.Map(
         location=start_latlon, 
@@ -17,11 +19,40 @@ def index():
         tiles="CartoDB Positron"
     )
 
+    # color dictionary
+    pfas_desc_colors = {
+        pfas_desc: color 
+        for pfas_desc, color in zip(pfas["pfas_desc"].unique(), ["#123f5a", "#7bbcb0", "#235d72", "#3a7c89"])
+    }
+
+    # markers + popups
     for index, row in pfas.iterrows():
-        folium.Marker(
+        folium.CircleMarker(
             location=[row["latitude"], row["longitude"]],
-            popup = f"Name: {row['site_name']}<br>State: {row['state']}").add_to(folium_map)
-        
+            radius=4,
+            color=pfas_desc_colors[row["pfas_desc"]],
+            fill=True,
+            fill_color=pfas_desc_colors[row["pfas_desc"]],
+            fill_opacity = 0.5,
+            popup = f"<b>Name:</b> {row['site_name']}<br><b>Industry:</b> {row['industry']}<br><b>Total PFAS (ng/L):</b> {row['pfas_desc']}<br><b>Address:</b> {row['number']} {row['street']} {row['city']}, {row['state']} {row['geocoded_zip']}", max_width=300).add_to(folium_map)
+
+    # legend    
+    legend_html = """
+    <div style="position: fixed; 
+                bottom: 50px; left: 50px; width: 150px; height: 165px; 
+                border:2px solid white; z-index:9999; font-size:12px;
+                background-color: rgba(255, 255, 255, 0.75); font-family: 'Source Sans Pro', sans-serif;
+                ">
+    <div style='font-weight: bold; padding: 2px;'>Total PFAS (ng/L)</div>
+    <p style="margin:10px"><span style='background-color:#7bbcb0'>&nbsp;&nbsp;&nbsp;&nbsp;</span> Less than 10</p>
+    <p style="margin:10px"><span style='background-color:#3a7c89'>&nbsp;&nbsp;&nbsp;&nbsp;</span> Between 10 and 1,000</p>
+    <p style="margin:10px"><span style='background-color:#235d72'>&nbsp;&nbsp;&nbsp;&nbsp;</span> Between 1,000 and 100,000</p>
+    <p style="margin:10px"><span style='background-color:#123f5a'>&nbsp;&nbsp;&nbsp;&nbsp;</span> Greater than 100,000</p>
+    </div>
+    """
+    folium_map.get_root().html.add_child(folium.Element(legend_html))
+
+    # geo search 
     plugins.Geocoder().add_to(folium_map)
 
     folium_map.save('templates/map.html')
